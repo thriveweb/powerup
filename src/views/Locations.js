5bf328e44ff18b723cc035afb073eba394fd6fd5
Alex <alex@thriveweb.com.au>
2018-04-11 12:12:31 +1000

    map and staff

--- src/views/Locations.js
+++ src/views/Locations.js
@@ -5,23 +5,28 @@ import PageHeader from '../components/PageHeader'
 import LocationItem from '../components/LocationItem'
 import './Locations.css'
 
-export default ({ page }) => (
-  <main className='Location' data-aos='fade-up'>
-    <Helmet>
-      <title>{page.title}</title>
-    </Helmet>
-    <PageHeader title={page.title} bannerImage={page.bannerImage} />
-    <section className='section'>
-      <div className='container'>
-        {page.subtitle && <h2 className='Locations--title'>{page.subtitle}</h2>}
-        {page.thumbnailLocations && (
-          <div className='Locations--Grid'>
-            {page.thumbnailLocations.map((locationItem, index) => (
-              <LocationItem key={index} locationItem={locationItem} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
-  </main>
-)
+const Locations = ({ page, ...props }) => {
+  return (
+    <main className='Location' data-aos='fade-up'>
+      <Helmet>
+        <title>{page.title}</title>
+      </Helmet>
+      <PageHeader title={page.title} bannerImage={page.bannerImage} />
+      <section className='section'>
+        <div className='container'>
+          {page.subtitle && (
+            <h2 className='Locations--title'>{page.subtitle}</h2>
+          )}
+          {page.thumbnailLocations && (
+            <div className='Locations--Grid'>
+              {page.thumbnailLocations.map((locationItem, index) => (
+                <LocationItem key={index} locationItem={locationItem} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </main>
+  )
+}
+export default Locations
--- src/views/SingleLocation.js
+++ src/views/SingleLocation.js
@@ -1,67 +1,134 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { Link } from 'react-router-dom'
 
-import Content from '../components/Content'
-import Gallery from '../components/Gallery'
+import MapBanner from '../components/MapBanner'
+import PageHeader from '../components/PageHeader'
+import BackgroundImage from '../components/BackgroundImage'
 
 import './SingleLocation.css'
 
-export default ({ singleLocation }) => (
-  <article className='Location' data-aos='fade-up'>
-    <Helmet>
-      <title>{singleLocation.title}</title>
-    </Helmet>
+const SingleLocation = ({ singleLocation, ...props }) => {
+  return (
+    <article className='Location' data-aos='fade-up'>
+      <Helmet>
+        <title>{singleLocation.title}</title>
+      </Helmet>
+      <PageHeader
+        title={singleLocation.title}
+        bannerImage={singleLocation.bannerImage}
+      />
+      <section className='section SingleLocation relative'>
+        <div className='container'>
+          <div className='SingleLocation--content'>
+            {singleLocation.title && (
+              <h2 className='SingleLocation--content--title'>
+                {singleLocation.title}
+              </h2>
+            )}
+            {singleLocation.subTitle && <h2>{singleLocation.subTitle}</h2>}
+            {singleLocation.location.address && (
+              <p>{singleLocation.location.address}</p>
+            )}
+            {singleLocation.classInfo && <p>{singleLocation.classInfo}</p>}
 
-    <section className='section thin'>
-      <div className='container Flex alignStart justifyBetween'>
-        {singleLocation.projectGalleryImages && (
-          <div className='SingleLocation--gallery'>
-            <Gallery
-              images={singleLocation.projectGalleryImages.map(
-                obj => obj.galleryimage
+            {singleLocation.bookingLink && (
+              <Link className='button' to={singleLocation.bookingLink}>
+                Book a class
+              </Link>
+            )}
+            <div className='social--container'>
+              <p>Follow studio</p>
+              {singleLocation.twitterURL && (
+                <Link target='_blank' to={singleLocation.twitterURL}>
+                  <img src='/images/twitter.svg' alt='twitter' />
+                </Link>
               )}
+              {singleLocation.facebookURL && (
+                <Link target='_blank' to={singleLocation.facebookURL}>
+                  <img src='/images/facebook.svg' alt='facebook' />
+                </Link>
+              )}
+              {singleLocation.linkedinURL && (
+                <Link target='_blank' to={singleLocation.linkedinURL}>
+                  <img src='/images/linkedin.svg' alt='linkedin' />
+                </Link>
+              )}
+              {singleLocation.instagramURL && (
+                <Link target='_blank' to={singleLocation.instagramURL}>
+                  <img src='/images/instagram.svg' alt='instagram' />
+                </Link>
+              )}
+            </div>
+          </div>
+          <div className='SingleLocation--thumbnail square'>
+            <BackgroundImage
+              className='square-content'
+              src={singleLocation.featuredThumbnail}
             />
           </div>
-        )}
-        <div className='SingleLocation--content'>
-          {singleLocation.title && <h1>{singleLocation.title}</h1>}
-          {singleLocation.content && (
-            <Content source={singleLocation.content} />
+        </div>
+      </section>
+      <section className='section Links'>
+        <div className='container'>
+          {singleLocation.bookingLink && (
+            <Link to={singleLocation.bookingLink}>
+              <img src='/images/instagram.svg' alt='instagram' /> Book a class
+            </Link>
           )}
-          <div className='breakout Flex alignStart justifyBetween'>
-            {singleLocation.year && (
-              <div className='year breakout--box'>
-                <h3>Year</h3>
-                <p>{singleLocation.year}</p>
-              </div>
-            )}
-            {singleLocation.typeofproject && (
-              <div className='type breakout--box'>
-                <h3>Type of project</h3>
-                <p>{singleLocation.typeofproject}</p>
-              </div>
-            )}
-            {singleLocation.client && (
-              <div className='client breakout--box'>
-                <h3>Client</h3>
-                <p>{singleLocation.client}</p>
-              </div>
-            )}
-          </div>
 
-          {singleLocation.testimonial && (
-            <div className='testimonials'>
-              <h3>Client Testimonials</h3>
-              <em>&quot;</em>
-              <blockquote>{singleLocation.testimonial}</blockquote>
-              <div className='from Flex alignStart justifyStart'>
-                {singleLocation.name && <p>{singleLocation.name}</p>}
-                {singleLocation.role && <small>{singleLocation.role}</small>}
-              </div>
-            </div>
+          {singleLocation.location.address && (
+            <Link
+              to={`https://www.google.com/maps/dir//${
+                singleLocation.location.address
+              }`}
+            >
+              <img src='/images/instagram.svg' alt='instagram' /> Map it
+            </Link>
+          )}
+          {singleLocation.phone && (
+            <Link to={`tel:${singleLocation.phone}`}>
+              <img src='/images/instagram.svg' alt='instagram' /> Call studio
+            </Link>
+          )}
+          {singleLocation.email && (
+            <Link to={`mailto:${singleLocation.email}`}>
+              <img src='/images/instagram.svg' alt='instagram' /> Email studio
+            </Link>
           )}
         </div>
-      </div>
-    </section>
-  </article>
-)
+      </section>
+      {singleLocation.location.lat && (
+        <div className='section thin MapBanner'>
+          <MapBanner
+            apiKey='AIzaSyCcfv8L8FmeieABBF2u1dZxeB3NlULe_Nw'
+            lat={singleLocation.location.lat}
+            lng={singleLocation.location.lng}
+          />
+        </div>
+      )}
+      {singleLocation.staff && (
+        <section className='section Staff'>
+          <div className='container'>
+            {singleLocation.staff.map((member, index) => (
+              <div key={member.name + index} className='Staff--Menber square'>
+                <div className='square'>
+                  <BackgroundImage
+                    className='square-content'
+                    src={member.image}
+                  />
+                  <div className='Staff--Menber--name'>{member.name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      )}
+
+      <section className='section Banner'>
+        <div className='container'>banner</div>
+      </section>
+    </article>
+  )
+}
+export default SingleLocation
