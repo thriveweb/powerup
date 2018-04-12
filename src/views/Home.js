30ccdfb4db01970ef24d7eb7fb674f193b5fe59f
Alex <alex@thriveweb.com.au>
2018-04-12 14:28:36 +1000

    home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -25,6 +25,71 @@ const Home = ({ page, services }) => {
         </section>
       )}
 
+      {page.welcomeImage && (
+        <div className='welcomeImage'> {page.welcomeImage}</div>
+      )}
+
+      {page.title && <div className='title'> {page.title}</div>}
+
+      {page.buttons.label && <div className='label'> {page.buttons.label}</div>}
+      {page.buttons.link && <div className='link'> {page.buttons.link}</div>}
+
+      {page.classesSectionTitle && (
+        <div className='classesSectionTitle'> {page.classesSectionTitle}</div>
+      )}
+
+      {page.pods.title && <div className='title'> {page.pods.title}</div>}
+
+      {page.pods.description && (
+        <div className='description'> {page.pods.description}</div>
+      )}
+
+      {page.pods.label && <div className='url'> {page.pods.label}</div>}
+      {page.pods.url && <div className='url'> {page.pods.url}</div>}
+
+      {page.servicesSubtitle && (
+        <div className='servicesSubtitle'> {page.servicesSubtitle}</div>
+      )}
+
+      {page.servicesContent && (
+        <div className='servicesContent'> {page.servicesContent}</div>
+      )}
+
+      {page.servicesSectionTitle && (
+        <div className='servicesSectionTitle'> {page.servicesSectionTitle}</div>
+      )}
+
+      {page.servicesSubtitle && (
+        <div className='servicesSubtitle'> {page.servicesSubtitle}</div>
+      )}
+
+      {page.servicesContent && (
+        <div className='servicesContent'> {page.servicesContent}</div>
+      )}
+
+      {page.testimonialBanner && (
+        <div className='testimonialBanner'> {page.testimonialBanner}</div>
+      )}
+
+      {page.testimonialSectionTitle && (
+        <div className='testimonialSectionTitle'>
+          {' '}
+          {page.testimonialSectionTitle}
+        </div>
+      )}
+
+      {page.testimonialImage && (
+        <div className='testimonialImage'> {page.testimonialImage}</div>
+      )}
+
+      {page.testimonialThumbnail && (
+        <div className='testimonialThumbnail'> {page.testimonialThumbnail}</div>
+      )}
+
+      {page.testimonial && (
+        <div className='testimonial'> {page.testimonial}</div>
+      )}
+
       {services && (
         <ServicePodSection title={page.servicesTitle} services={services} />
       )}
