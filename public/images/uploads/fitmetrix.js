4f859f15813c3067d2738774f2a3e8a4b57b29e1
rajeshd <rajeshd@fitmetrix.io>
2018-06-12 13:44:28 +0000

    Upload /public/images/uploads/fitmetrix.js

---
+++ public/images/uploads/fitmetrix.js
@@ -0,0 +1,18 @@
+﻿jQuery(document).ready(function () {
+    if (jQuery('iframe#sf-frame').length == 1) {
+        var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1); // Detect IE10 and below
+        jQuery('iframe#sf-frame').iFrameResize({
+            log: false,
+            checkOrigin: false,
+            enablePublicMethods: true,
+            minHeight: 768,
+            heightCalculationMethod: isOldIE ? 'max' : 'lowestElement'
+        });
+    }
+});
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
